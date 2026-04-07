194bf2a842d39afc066330128da1214ccaaab77f
Magaiver <magaivermagalhaes.mm@gmail.com>
2026-04-07 14:43:06 -0300

    fix: configure tailwind and implement global design tokens

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        background: 'rgb(var(--background))',
+        foreground: 'rgb(var(--foreground))',
+        primary: 'rgb(var(--primary))',
+      },
+    },
+  },
+  plugins: [],
+};
